c54f1a338382e4834b343a94013aa88ada6562e3
Giamme <39541140+Giamme@users.noreply.github.com>
2024-11-26 22:53:57 +0100

    feat: key value database for fast development

--- database/index.ts
+++ database/index.ts
@@ -1,9 +1,21 @@
-import { Database} from 'duckdb-async';
+import { Database } from "duckdb-async";
+import Keyv from "@keyvhq/core";
+import { LocalDatabase } from "../src/types";
 
 async function initDatabase(): Promise<Database> {
-const db = await Database.create(':memory:');   
+  const db = await Database.create(":memory:");
 
-return db;
+  return db;
+}
+
+export function initLocalDatabase(): LocalDatabase {
+  const db: LocalDatabase = {
+    matches: new Keyv(),
+    players: new Keyv(),
+    league: new Keyv(),
+  };
+
+  return db;
 }
 
 export default initDatabase;
